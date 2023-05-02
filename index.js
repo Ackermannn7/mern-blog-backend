14772e3ea7ba95cb9cb6ca2371488300bc77d45a
Aleksey Rudyk <95295778+Ackermannn7@users.noreply.github.com>
2023-05-02 17:18:22 +0300

    bug fix

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ import { UserController, PostController } from "./controllers/index.js";
 import { handleValidationErrors, checkAuth } from "./utils/index.js";
 
 mongoose
-  .connect(process.env.MONGODB_URI)
+  .connect(`${process.env.MONGODB_URI}`)
   .then(() => console.log("DB connected successfully!"))
   .catch((err) => console.log("DB error!", err));
 
